edf6d0e65148c2bcd2b7e136bbaf6e12152c7857
Bryce <vehicola@gmail.com>
2017-03-22 13:03:20 -0700

    Delete Engine.js

--- sonic/js/Engine.js
+++
@@ -1,8 +0,0 @@
-var Engine = function(canvas) {
-	var engine = {}
-	engine.gfx = GraphicsEngine(engine, canvas);
-	engine.resources = ResourceManager(engine);
-	engine.world = WorldEngine(engine);
-	engine.input = InputController(engine);
-	return engine;
-}
